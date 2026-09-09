3668a8335b99a117e0b60527c16c7d033fa5bce3
Hasan Alqallaf <hheamg2005@gmail.com>
2026-09-09 14:24:47 +0300

    first commit

---
+++ src/controllers/watchlistController.js
@@ -0,0 +1,111 @@
+import { prisma } from "../config/db.js";
+
+const addToWatchlist = async (req, res) => {
+  const { movieId, status, rating, notes } = req.body;
+
+  const movie = await prisma.movie.findUnique({
+    where: { id: movieId },
+  });
+
+  if (!movie) {
+    return res.status(404).json({ error: "Movie not found" });
+  }
+
+  const existInWatchlist = await prisma.watchlistItem.findUnique({
+    where: {
+      userId_movieId: {
+        userId: req.user.id,
+        movieId: movieId,
+      },
+    },
+  });
+
+  if (existInWatchlist) {
+    return res.status(400).json({ error: "Movie already in the watchlist" });
+  }
+
+  const watchlistItem = await prisma.watchlistItem.create({
+    data: {
+      userId: req.user.id,
+      movieId,
+      status: status || "PLANNED",
+      rating,
+      notes,
+    },
+  });
+
+  res.status(201).json({
+    status: "Success",
+    data: {
+      watchlistItem,
+    },
+  });
+};
+
+const removeFromWatchlist = async (req, res) => {
+  // Find watchlist item and verify ownership
+  const watchlistItem = await prisma.watchlistItem.findUnique({
+    where: { id: req.params.id },
+  });
+
+  if (!watchlistItem) {
+    return res.status(404).json({ error: "Watchlist item not found" });
+  }
+
+  // Ensure only owner can delete
+  if (watchlistItem.userId !== req.user.id) {
+    return res
+      .status(403)
+      .json({ error: "Not allowed to update this watchlist item" });
+  }
+
+  await prisma.watchlistItem.delete({
+    where: { id: req.params.id },
+  });
+
+  res.status(200).json({
+    status: "Success",
+    message: "Movie remvoed from watchlist",
+  });
+};
+
+const updateWatchlistItem = async (req, res) => {
+  const { status, rating, notes } = req.body;
+
+  // Find watchlist item and verify ownership
+  const watchlistItem = await prisma.watchlistItem.findUnique({
+    where: { id: req.params.id },
+  });
+
+  if (!watchlistItem) {
+    return res.status(404).json({ error: "Watchlist item not found" });
+  }
+
+  // Ensure only owner can update
+  if (watchlistItem.userId !== req.user.id) {
+    return res
+      .status(403)
+      .json({ error: "Not allowed to update this watchlist item" });
+  }
+
+  // Build update data
+  const updateData = {};
+  if (status !== undefined) updateData.status = status.toUpperCase();
+  if (rating !== undefined) updateData.rating = rating;
+  if (notes !== undefined) updateData.notes = notes;
+
+  // Update watchlist item
+  const updateItem = await prisma.watchlistItem.update({
+    where: { id: req.params.id },
+    data: updateData,
+  });
+
+  res.status(200).json({
+    status: "success",
+    date: {
+      watchlistItem: updateItem,
+    },
+  });
+};
+
+export { addToWatchlist, removeFromWatchlist, updateWatchlistItem };
